8bcae1259e49884466d24bb30b5e62c661a3e68d
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-03 23:33:43 +0700

    menambahkan componen map(leaflet dan navbar

--- Knowledge/src/App.jsx
+++ Knowledge/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
-
+import { Box, ChakraProvider } from '@chakra-ui/react';
+import Hero from './components/Hero';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 
 function App() {
-  return <div className="App">Cari Bencana</div>;
+  return (
+    <BrowserRouter>
+      <ChakraProvider>
+        <Box minHeight={'100vh'}>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+          </Routes>
+        </Box>
+      </ChakraProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Knowledge/src/components/Hero.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import MapHero from './leaflet/MapHero';
+import cari from '../assets/cari-logo.png';
+function Hero() {
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setToggle(true);
+      } else {
+        setToggle(false);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleNav = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className="relative">
+      <nav className={`absolute flex items-center justify-between flex-wrap top-5 right-5 z-20 w-2/4 p-6 bg-zinc-800/25    hover:-translate-y-2  transition transform rounded-md  ${toggle ? 'block' : 'hidden'}`}>
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <a href="https://caribencana.id/">
+            <img className="fill-current  w-30 mr-2" width="80" height="80" viewBox="0 0 54 54" src={cari} alt="CARI Website"></img>
+          </a>
+        </div>
+
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4 hover:text-zinc-800/75">
+              Home
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4 hover:text-zinc-800/75">
+              Knowledge Portal
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white  mr-4 hover:text-zinc-800/75">
+              Publications
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4 hover:text-zinc-800/75">
+              Special Brief
+            </a>
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-zinc-800/75">
+              About
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+              Login
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="relative top-0 left-0 h-500 w-full z-10 outline-none">
+        <MapHero />
+      </div>
+      <button className={`absolute top-2 right-2 z-20 bg-gray-900 hover:bg-gray-800  transition transform rounded-md focus:outline-none ${toggle ? 'hidden' : 'block'}`} onClick={toggleNav}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default Hero;
