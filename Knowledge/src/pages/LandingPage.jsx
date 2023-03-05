177e154a493c508bb5641caca195a78724cb7e7e
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-05 09:29:47 +0700

    menambahkan halaman subscription & edit halaman footer

--- Knowledge/src/pages/LandingPage.jsx
+++ Knowledge/src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-
+import Subscription from '../components/Subscription';
 function LandingPage() {
   return (
     <div>
       <Hero />
+      <Subscription />
       <Footer />
     </div>
   );
